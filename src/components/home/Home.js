2bd6b3e6e007e7dba00db311590229cd184ebfd2
Bito Mann <bito@bitomann.com>
2020-03-13 17:12:00 -0500

    created navbar application-views home

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="center">
+            <h1 className="centerMe">VQ-Planner</h1>
+            <img src={require("../images/Screenshot 2020-02-28 11.03.05.png")} alt="f3 logo"></img>
+        </div>
+    )
+}
+
+export default Home
